8fa966e8bed9fefa6378e2848c771e97a6132c68
William Wells <whanwells@gmail.com>
2021-04-10 10:24:59 -0500

    Fade in when loaded

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,17 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { checkSession } from "./api";
 import Navbar from "./Navbar.jsx";
+import "./App.css";
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
-    checkSession().then((data) => console.log(data));
+    checkSession().then((data) => {
+      console.log(data);
+      setLoading(false);
+    });
   });
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className="container"></div>
+      <div className="app">
+        <Navbar />
+        <div className="container"></div>
+      </div>
     </BrowserRouter>
   );
 };
