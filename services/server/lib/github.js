2dfe42c4bf2e8372c27d45ceb83eb953061b2638
William Wells <whanwells@gmail.com>
2021-04-10 18:17:47 -0500

    Tie user to GitHub profile

--- services/server/lib/github.js
+++ services/server/lib/github.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
+import { Strategy } from "passport-github";
+import { addUserByGitHubId, findUserByGitHubId } from "./db.js";
 
 const {
   APP_BASE_URL,
@@ -8,13 +10,30 @@ const {
   GITHUB_CALLBACK_PATH,
 } = process.env;
 
-export const GITHUB_STRATEGY_CONFIG = {
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: GITHUB_CALLBACK_PATH,
-};
+export function githubStrategy() {
+  return new Strategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: GITHUB_CALLBACK_PATH,
+    },
+    async (accessToken, refreshToken, profile, next) => {
+      try {
+        let id = await findUserByGitHubId(profile.id);
 
-export function githubLoginRouter() {
+        if (!id) {
+          id = await addUserByGitHubId(profile.id);
+        }
+
+        next(null, id);
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+}
+
+export function githubRouter() {
   const router = Router();
 
   router.get("/login/github", passport.authenticate("github"));
--- services/server/lib/index.js
+++ services/server/lib/index.js
@@ -2,26 +2,13 @@
 import bodyParser from "body-parser";
 import express from "express";
 import passport from "passport";
-import { Strategy as GitHubStrategy } from "passport-github";
 import { apiRouter } from "./api.js";
-import { GITHUB_STRATEGY_CONFIG, githubLoginRouter } from "./github.js";
+import { githubStrategy, githubRouter } from "./github.js";
 import session from "./session.js";
 
-// https://github.com/passport/express-4.x-facebook-example/blob/master/server.js
-passport.use(
-  new GitHubStrategy(
-    GITHUB_STRATEGY_CONFIG,
-    (accessToken, refreshToken, profile, cb) => cb(null, profile)
-  )
-);
-
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-  cb(null, obj);
-});
+passport.use(githubStrategy());
+passport.serializeUser((id, done) => done(null, id));
+passport.deserializeUser((id, done) => done(null, id));
 
 const app = express();
 app.use(express.static("public"));
@@ -30,7 +17,7 @@ app.use(session());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(apiRouter());
-app.use(githubLoginRouter());
+app.use(githubRouter());
 
 const port = 3000;
 
