e74e3575a6837e9153ee8c872f5ee6a802df172d
William Wells <whanwells@gmail.com>
2021-04-11 00:13:05 -0500

    Cleanup imports

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,8 +1,6 @@
-import "bootstrap/dist/js/bootstrap.bundle.js";
-import "bootstrap/dist/css/bootstrap.css";
 import { render } from "react-dom";
 import App from "./App.jsx";
+import "bootstrap";
 import "./main.css";
-
 
 render(<App />, document.getElementById("root"));
