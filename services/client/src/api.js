5983757627b977f3ccc4d8cec1c44bbb2d85903d
William Wells <whanwells@gmail.com>
2021-04-11 01:07:56 -0500

    Factor out request function

--- services/client/src/api.js
+++ services/client/src/api.js
@@ -1,8 +1,13 @@
 const EXPRESS_BASE_URL = process.env.EXPRESS_BASE_URL;
 
-export async function checkSession() {
-  const response = await fetch(`${EXPRESS_BASE_URL}/session`, {
+function request(path, options) {
+  return fetch(`${EXPRESS_BASE_URL}${path}`, {
     credentials: "include",
+    ...options,
   });
+}
+
+export async function checkSession() {
+  const response = await request("/session");
   return response.json();
 }
