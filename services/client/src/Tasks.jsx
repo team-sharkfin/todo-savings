d1acc86c817c8258cbeeeb7ad615d80926aa05f7
borng-osu <62971727+borng-osu@users.noreply.github.com>
2021-04-11 12:32:44 -0400

    SQL for Tasks

--- services/client/src/Tasks.jsx
+++ services/client/src/Tasks.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import PropTypes from "prop-types";
 import Banner from "./Banner.jsx";
 
@@ -26,6 +27,67 @@ const TaskTable = () => (
     </tbody>
   </table>
 );
+=======
+import { useQuery } from "react-query";
+import { getTasks } from "./api";
+import Banner from "./Banner.jsx";
+import RewardForm from "./RewardForm";
+
+const Tasks = () => {
+  const { isLoading, data } = useQuery("tasks", getTasks);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <>
+      <Banner>Upcoming Tasks</Banner>
+      <div className="row g-0">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Task</th>
+              <th scope="col">Reward</th>
+              <th scope="col">Complete?</th>
+            </tr>
+          </thead>
+          {data.length && (
+            <tbody>
+              {data.map(({ taskId, name, reward, amountPerTask }) => {
+                return (
+                  <tr>
+                    <th scope="row">{name}</th>
+                    <td>
+                      ${amountPerTask} towards {reward}
+                    </td>
+                    <td>
+                      <div
+                        className="btn-group"
+                        role="group"
+                        aria-label="Basic mixed styles example"
+                      >
+                        <button
+                          type="button"
+                          className="btn btn-success"
+                          onClick={"completeTask(" + taskId + ")"}
+                        >
+                          Done
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          )}
+        </table>
+      </div>
+      <RewardForm />
+    </>
+  );
+};
+>>>>>>> Stashed changes
 
 const ToDo = (props) => (
   <tr>
@@ -41,6 +103,7 @@ const ToDo = (props) => (
       >
         <button
           type="button"
+<<<<<<< Updated upstream
           className="btn btn-warning"
           onClick={"updateTask(" + props.id + ")"}
         >
@@ -52,12 +115,19 @@ const ToDo = (props) => (
           onClick={"completeTask(" + props.id + ")"}
         >
           Complete
+=======
+          className="btn btn-success"
+          onClick={"completeTask(" + props.id + ")"}
+        >
+          Done
+>>>>>>> Stashed changes
         </button>
       </div>
     </td>
   </tr>
 );
 
+<<<<<<< Updated upstream
 ToDo.propTypes = {
   id: PropTypes.string,
   task: PropTypes.string,
@@ -65,4 +135,6 @@ ToDo.propTypes = {
   due: PropTypes.string,
 };
 
+=======
+>>>>>>> Stashed changes
 export default Tasks;
